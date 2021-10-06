27f4b48f89a0503f99b9688a1fc72764a0bd37ce
Mskerz <59002086+mskerz@users.noreply.github.com>
2021-10-06 22:20:50 +0700

    Update Function 1&2.c

--- Function 1&2.c
+++ Function 1&2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <math.h>
-int get_Fx(int a){     /////1
+int get_Fx(int a){     /////1 ฟังก์ชั่น f(x)
 	int fx;
 	printf("enter x:");
 	scanf("%d",&a);
 	if(a<0){
 		printf("condition 1\n");
 		fx=3*pow(a,2)+2*a+7;
-		return fx;
-	}
+		return fx;                        // return fx ไปในทุกๆเงื่อนไข  เพื่อส่งค่ากลับไปยัง parametersของฟังก์ชั่น get_Fx             
+	} 
 	if(a==0){
 		printf("condition 2\n");
 		fx=0;
@@ -21,16 +21,16 @@ int get_Fx(int a){     /////1
 	}
 	
 }
-int Get_Fxy(int a,int b){    ///// 2
+int Get_Fxy(int a,int b){    ///// 2 ฟังก์ชั่น f(x,y)
 	int f_xy;
 	printf("enter x:");
 	scanf("%d",&a);
 	printf("enter y:");
 	scanf("%d",&b);
 	if(a+b>0){
-		printf("condition 1\n");
+		printf("condition 1\n");  
 		f_xy=a+b;
-		return f_xy;
+		return f_xy;                      // return f_xy ไปในทุกๆเงื่อนไข  เพื่อส่งค่ากลับไปยัง parametersของฟังก์ชั่น  Get_Fxy
 	}
 	if(a+b==0){
 		printf("condition 2\n");
