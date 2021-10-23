ef693d3ce22d99538457b7fd6d1d2e4a391730d1
Mskerz <59002086+mskerz@users.noreply.github.com>
2021-10-23 11:36:40 +0700

    Update 3.c

--- โจทย์ฝึกทำ 20-10-64/3.c
+++ โจทย์ฝึกทำ 20-10-64/3.c
@@ -12,8 +12,7 @@ main(){
 	for(i=1;i<=10;i++){
 		if(nums==num[i]){
 			printf("Found Number:%d at Position %d\n",nums,i);
-		}/*else if(nums!=num[0]&&nums!=num[1]&&nums!=num[2]&&nums!=num[3]&&nums!=num[4]&&nums!=num[5]&&nums!=num[6]&&nums!=num[7]&&nums!=num[8]&&nums!=num[9]){
-			printf("not Found!");*/
+		}
 		
 	}if(nums!=num[i]){
 			printf("Number not Found!");
